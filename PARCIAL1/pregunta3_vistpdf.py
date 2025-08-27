ce6659f1c184f0acba183f3e579ac7a5b051bc96
Jimena <jimenamartinezoldani@gmail.com>
2025-08-27 18:02:25 -0300

    #2-Pregunta3-vistapdf

---
+++ PARCIAL1/pregunta3_vistpdf.py
@@ -0,0 +1,67 @@
+from flask import Blueprint, request, jsonify, Response
+from app.services.alumno_service import AlumnoService
+from app.mapping.alumno_mapping import AlumnoMapping
+from app.validators.alumno_validator import validate_alumno
+
+alumno_bp = Blueprint('alumno', __name__)
+alumno_mapping = AlumnoMapping()
+
+@alumno_bp.route('/alumnos', methods=['GET'])
+def read_all():
+    alumnos = AlumnoService.buscar_todos()
+    return alumno_mapping.dump(alumnos, many=True), 200
+
+@alumno_bp.route('/alumno/<hashid:id>', methods=['GET'])
+def read_by_id(id: int):
+    alumno = AlumnoService.buscar_por_id(id)
+    if not alumno:
+        return jsonify({"error": "Alumno no encontrado"}), 404
+    return alumno_mapping.dump(alumno), 200
+
+@alumno_bp.route('/alumno', methods=['POST'])
+def create():
+    data = request.get_json()
+    errors = validate_alumno(data)
+    if errors:
+        return jsonify({'errors': errors}), 400
+    try:
+        nuevo_alumno = AlumnoService.crear_alumno(data)
+        return alumno_mapping.dump(nuevo_alumno), 201
+    except ValueError as e:
+        return jsonify({"error": str(e)}), 400
+
+@alumno_bp.route('/alumno/<hashid:id>', methods=['PUT'])
+def update(id: int):
+    data = request.get_json()
+    errors = validate_alumno(data)
+    if errors:
+        return jsonify({"errors": errors}), 400
+    
+    alumno_actualizado = AlumnoService.actualizar_alumno(id, data)
+    if not alumno_actualizado:
+        return jsonify({"error": "Alumno no encontrado"}), 404
+    return alumno_mapping.dump(alumno_actualizado), 200
+
+@alumno_bp.route('/alumno/<hashid:id>', methods=['DELETE'])
+def delete(id: int):
+    eliminado = AlumnoService.borrar_alumno(id)
+    if not eliminado:
+        return jsonify({"error": "Alumno no encontrado"}), 404
+    return jsonify({"message": "Alumno eliminado"}), 200
+
+@alumno_bp.route('/alumno/<hashid:id>/ficha', methods=['GET'])
+def ficha_alumno(id: int):
+    alumno = AlumnoService.buscar_por_id(id)
+    if not alumno:
+        return jsonify({"error": "Alumno no encontrado"}), 404
+    formato = request.args.get("format", "json") 
+    if formato == "pdf":
+        pdf_bytes = AlumnoService.generar_ficha_pdf(alumno)
+        return Response(
+            pdf_bytes,
+            mimetype="application/pdf",
+            headers={"Content-Disposition": f"inline; filename=ficha_alumno_{id}.pdf"}
+        )
+
+    return alumno_mapping.dump(alumno), 200
+#VISTA PDF
